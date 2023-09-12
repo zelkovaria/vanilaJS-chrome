c77914f7bfcdb2bfde73427adfcd4619f517cd2f
zelkovaria <hyein396@gmail.com>
2023-09-12 21:14:28 +0900

    localstorage username 유무에 따른 동작

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("input");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
 }
-
-function handleLinClick(event) {
-  event.preventDefault();
-  console.dir(event);
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-handleLinkClick({});
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
