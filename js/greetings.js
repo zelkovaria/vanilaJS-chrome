649259ff3f5992a58022a695243743fbb70f19d7
zelkovaria <hyein396@gmail.com>
2023-09-16 01:23:51 +0900

    interval, padStart를 사용한 시계 표시

--- js/greetings.js
+++ js/greetings.js
@@ -10,17 +10,19 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  paintGreeting(username);
+  paintGreetings(username);
 }
-function paintGreeting(username) {
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintGreeting(savedUsername);
+  paintGreetings(savedUsername);
 }
